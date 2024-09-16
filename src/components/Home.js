d875fe96cf1cfcb8d676cd41f947029c8c60541c
ChauHuyDien <82036270+idiotman-2212@users.noreply.github.com>
2024-09-16 15:12:27 +0700

    update home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
 const Home = () => {
   return (
     <>
-      <div className="home-container"> Hello</div>
+      <div className="home-container">
+      <h2>Yêu cầu:</h2>
+    <p>Sử dụng API từ trang web <a href="http://reqres.in/">http://reqres.in/</a> để tạo website.</p>
+    <p>Sử dụng thư viện React để tạo một màn hình website bao gồm các chức năng:</p>
+    
+    <ol>
+        <li>Đăng nhập</li>
+        <li>Thêm User</li>
+        <li>Sửa User</li>
+        <li>Xóa User</li>
+        <li>Hiển thị tất cả User</li>
+        <li>Tìm kiếm User theo id</li>
+        <li>Sắp xếp theo FirstName</li>
+    </ol>
+    
+    <p>Tự do tùy chỉnh HTML, CSS để có một website nhẹ nhàng, khoa học và đẹp.</p>
+</div>
     </>
   );
 };
